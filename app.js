fde69b3410271f37c5203a547336cbb60e3d20c1
Chris Carter <chris.j.carter91@gmail.com>
2020-04-17 00:36:10 -0600

    Added notes for data types

---
+++ app.js
@@ -0,0 +1,28 @@
+//Primitive Types
+
+// Strings
+const name = 'John Doe';
+// Number
+const age = 29;
+// Boolean
+const hasKids = true;
+// NUll
+const car = null;
+// Undefined 
+let test;
+// Symbol
+const sym = Symbol();
+
+// Reference Types
+
+// Array
+const hobbies = ['movies', 'music'];
+// Object Literal
+const address = {
+    city: 'Boston',
+    state: 'MA',
+}
+const today = new Date();
+console.log(today);
+
+console.log(typeof today);
